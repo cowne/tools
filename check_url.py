7717d05cb87c0be14332b5cbf7404432670c3402
cownee <hungthinh77mp@gmail.com>
2024-04-14 18:58:15 +0700

    Upload check_url.py

---
+++ check_url.py
@@ -0,0 +1,57 @@
+import requests
+import urllib3
+import argparse
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+parser = argparse.ArgumentParser(description="Checking available URL")
+parser.add_argument('-l','--list', type=str, help='List of URL',required=True)
+parser.add_argument('-o', '--output', type=str, help='Output file contain list available URL')
+args = parser.parse_args()
+
+def getURL(file):
+    urls = []
+    with open(file, 'r') as url_lists:
+        for url in url_lists:
+            urls.append(url.strip('\n'))
+    return urls
+
+def write_to_file(urls,file):
+    with open(file, 'a') as file:
+        for url in urls:
+            file.write(url+'\n')
+        file.close()
+
+def check_url(urls):
+    url_alive = []
+    url_death = {}
+    for url in urls:
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+        except Exception as e:
+            url_death.update({url: str(e)})
+        else:
+            url_alive.append(url)
+
+    return url_alive, url_death
+
+def main():
+    urls = getURL(args.list)
+    url_alive, url_death = check_url(urls)
+
+    if args.output:
+        write_to_file(url_alive, args.output)
+        print(f'[!] The reports save in {args.output}.')
+
+    print('\n------------URL AVAILABLE--------------------')
+    print(f'[!] The number domain available is {len(url_alive)}.')
+    for url in url_alive:
+        print(f'[+] {url}')
+
+    print('\n-------------URL DEATH-------------------')
+    print(f'[!] The number domain not available is {len(url_death)}.')
+    for url,error in url_death.items():
+        print(f'[-] {url}: {error}')
+
+if __name__ == '__main__':
+    main()
